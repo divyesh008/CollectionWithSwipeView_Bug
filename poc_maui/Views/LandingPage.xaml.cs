3235c1c0cabf341c0318c1cdc47a57fd1cb7e204
Divyesh Bhatt <divyesh.bhatt@tylertech.com>
2024-05-02 18:11:43 +0530

    Merged code

---
+++ poc_maui/Views/LandingPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace poc_maui.Views;
+
+public partial class LandingPage : ContentPage
+{
+	public LandingPage()
+	{
+		InitializeComponent();
+	}
+
+    void SwipeItem_Invoked(System.Object sender, System.EventArgs e)
+    {
+        Application.Current.MainPage.DisplayAlert("Alert", "Delete Command Triggered", "OK");
+    }
+}
